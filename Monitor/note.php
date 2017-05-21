6baf398da799c06c414dbff6292173d54741c763
Kylin <zc0k27@163.com>
2017-05-21 13:36:00 +0000

    增加备注和密码设置功能

--- Monitor/note.php
+++ Monitor/note.php
@@ -1,16 +1,26 @@
 <!doctype html>
 <html><head>
 <?php
-        $ip = $_GET['ip'];
-        $note = $_GET['note'];
+        $ip = $_POST['ip'];
+        $note = $_POST['note'];
+        $passwd = $_POST['password'];
         
         $new_name_tab = array(
-            $IP => $note
+            $ip => $note
             );
         $name_tab =  @json_decode(@file_get_contents('data/name.txt'), true);
         if (!$name_tab) {$name_tab = array();}
         $name_tab = array_replace($name_tab,$new_name_tab);
         file_put_contents('data/name.txt', json_encode($name_tab));
+        
+        $ip_name_tab = array('name' => $note);
+        $ip_tab = json_decode(file_get_contents('data/'.$ip.'.txt'), true);
+        $ip_tab = array_replace($ip_tab,$ip_name_tab);
+        if(!empty($passwd)){
+            $new_passwd_tab = array('passwd' => $passwd);
+            $ip_tab = array_replace($ip_tab,$new_passwd_tab);
+        }
+        file_put_contents('data/'.$ip.'.txt', json_encode($ip_tab));
         
         echo '<meta http-equiv="refresh" content="0; url=detal.php?ip=' . $ip . '">';
 ?>
